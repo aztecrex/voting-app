7699124da69fc60989ea5a87fe15fe2183ecf830
Greg Wiley <aztec.rex@jammm.com>
2016-04-12 17:14:49 -0700

    reducer

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+import {fromJS} from 'immutable'
+
+import {setEntries, next, vote} from './core';
+
+export default function reducer(state, action) {
+  switch (action.type) {
+  case 'SET_ENTRIES':
+    return setEntries(state, fromJS(action.entries));
+  case 'NEXT':
+    return next(state);
+  case 'VOTE':
+    return vote(state, action.entry)
+  }
+  return state;
+}
